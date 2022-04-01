ad21b17acf97f900d41d6659c3a010badfeb3a26
Nicole Trappe <ntrappe@ucsd.edu>
2022-04-01 14:36:40 -0700

    basic layout all done

---
+++ main.js
@@ -0,0 +1,11 @@
+const jobKeysContainer = document.getElementById ('job-keys');
+const microsoftBtn = document.getElementById ('msft-btn');
+const ibmBtn = document.getElementById ('ibm-btn');
+
+microsoftBtn.addEventListener ('click', () => {
+    console.log ('show microsoft');
+});
+
+ibmBtn.addEventListener ('click', () => {
+    console.log ('show ibm');
+});
